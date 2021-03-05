2e6f1e8d0b62ddcbde061090b494fe2071d18651
Jonathan Ryan <jonathanryan576@gmail.com>
2021-03-05 12:13:36 +0700

    exploring the connection between (romantic relationship and gender, age, parent status). Other connection are not yet done. Updating to do list. Saving a plot about gender and romantic variable

--- script/to do.R
+++ script/to do.R
@@ -1,15 +1,15 @@
 "To do list:
-1. 
+1. Cek jurnal ilmiah tentang pengaruh parental status terhadap kemungkinan
+seseorang berada pada hubungan romantis (pacaran)
 2. 
 
 Questions:
-1. Apakah perempuan atau laki2 yang lebih mungkin utk memiliki pacar? Pake var
-romantic
+1. Visualisasikan data probability romantic by age
 2. Apakah perempuan atau laki2 yang memiliki nilai lebih tinggi? Bandingin
 G1, G2, G3
 3. Apakah kita bisa memprediksi g3 hanya melalui g1 dan g2? Jadi tidak usah
 pake var2 lainnya
 5. Apakah punya internet meningkatkan nilai murid atau justru menurunkan? Liat
 var internet
-6. 
-
+6. Apakah internet mempengaruhi kemungkinan seorang anak untuk punya pacar?
+7. Apakah lama waktu belajar seorang murid mempengaruhi nilainya?
